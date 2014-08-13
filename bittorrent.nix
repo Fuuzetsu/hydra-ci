1f299eb89efb78063cb7ee2f747b41e7c4073af3
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-13 12:24:52 +0100

    I have no idea what the problem is.

--- bittorrent.nix
+++ bittorrent.nix
@@ -6,30 +6,30 @@ let
   genAttrs = (import <nixpkgs> {}).lib.genAttrs;
 in
 rec {
-  krpc = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-    in
-    haskellPackages.cabal.mkDerivation (self: {
-      pname = "krpc";
-      version = "HEAD";
-      src = <krpc>;
-      buildDepends = with haskellPackages; [
-        bencoding dataDefaultClass liftedBase monadControl monadLogger mtl
-        network text transformers
-      ];
-      testDepends = with haskellPackages; [
-        bencoding hspec monadLogger mtl network QuickCheck
-        quickcheckInstances
-      ];
-      meta = {
-        homepage = "https://github.com/cobit/krpc";
-        description = "KRPC protocol implementation";
-        license = self.stdenv.lib.licenses.bsd3;
-        platforms = self.ghc.meta.platforms;
-      };
-    })));
+  # krpc = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
+  #   let
+  #     pkgs = import <nixpkgs> { inherit system; };
+  #     haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
+  #   in
+  #   haskellPackages.cabal.mkDerivation (self: {
+  #     pname = "krpc";
+  #     version = "HEAD";
+  #     src = <krpc>;
+  #     buildDepends = with haskellPackages; [
+  #       bencoding dataDefaultClass liftedBase monadControl monadLogger mtl
+  #       network text transformers
+  #     ];
+  #     testDepends = with haskellPackages; [
+  #       bencoding hspec monadLogger mtl network QuickCheck
+  #       quickcheckInstances
+  #     ];
+  #     meta = {
+  #       homepage = "https://github.com/cobit/krpc";
+  #       description = "KRPC protocol implementation";
+  #       license = self.stdenv.lib.licenses.bsd3;
+  #       platforms = self.ghc.meta.platforms;
+  #     };
+  #   })));
 
   bencoding = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
     let
