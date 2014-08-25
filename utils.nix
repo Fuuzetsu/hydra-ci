d2407bc5925029ee8c208d8841ef449a77e1a75c
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-25 05:35:49 +0100

    semicolons…

--- utils.nix
+++ utils.nix
@@ -1,10 +1,10 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let strs = pkgs.lib.strings;
-    lines = strs.splitString "\n"
-    dropSpaces = strs.replaceChars [" "] [""]
-    findVersion = head (filter (strs.hasPrefix "version:"))
-    extractVersion = x: strs.removePrefix "version:" (dropSpaces x)
+    lines = strs.splitString "\n";
+    dropSpaces = strs.replaceChars [" "] [""];
+    findVersion = head (filter (strs.hasPrefix "version:"));
+    extractVersion = x: strs.removePrefix "version:" (dropSpaces x);
 
 inherit (builtins) readFile head filter;
 
