8f2c54629551bcc379166cb349a9d85b9096c1cc
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-25 06:21:33 +0100

    No compose ;_;

--- utils.nix
+++ utils.nix
@@ -3,7 +3,7 @@
 let strs = pkgs.lib.strings;
     lines = strs.splitString "\n";
     dropSpaces = strs.replaceChars [" "] [""];
-    findVersion = head (filter (strs.hasPrefix "version:"));
+    findVersion = x: head (filter (strs.hasPrefix "version:") x);
     extractVersion = x: strs.removePrefix "version:" (dropSpaces x);
 
 inherit (builtins) readFile head filter;
