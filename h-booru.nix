edf16e7a16cb5023ddad3d8f0bc0ad62b5733763
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 04:07:48 +0000

    h-booru: try re-using existing expr

--- h-booru.nix
+++ h-booru.nix
@@ -10,19 +10,23 @@ rec {
     let
       pkgs = import <nixpkgs> { inherit system; };
       haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-    in
-    haskellPackages.cabal.mkDerivation (self: {
-      pname = "h-booru";
-      version = "0.1.0.0";
-      src = <h-booru>;
-      isLibrary = true;
-      isExecutable = true;
-      buildDepends = with haskellPackages; [ httpConduit hxt utf8String vinyl ];
-      meta = {
-        homepage = "https://github.com/Fuuzetsu/h-booru";
-        description = "Haskell library for retrieving data from various booru image sites";
-        license = self.stdenv.lib.licenses.gpl3;
-        platforms = self.ghc.meta.platforms;
-      };
-    })));
+      srcLoc = <localexprs> + "/h-booru";
+    in pkgs.lib.overrideDerivation (haskellPackages.callPackage srcLoc {}) (attrs: {
+         src = <h-booru>;
+       }
+    # haskellPackages.cabal.mkDerivation (self: {
+    #   pname = "h-booru";
+    #   version = "0.1.0.0";
+    #   src = <h-booru>;
+    #   isLibrary = true;
+    #   isExecutable = true;
+    #   buildDepends = with haskellPackages; [ httpConduit hxt utf8String vinyl ];
+    #   meta = {
+    #     homepage = "https://github.com/Fuuzetsu/h-booru";
+    #     description = "Haskell library for retrieving data from various booru image sites";
+    #     license = self.stdenv.lib.licenses.gpl3;
+    #     platforms = self.ghc.meta.platforms;
+    #   };
+    # })
+    )));
 }
