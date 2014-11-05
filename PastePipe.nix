601b97fd1b98d035022bb34787d54c7b452578a6
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 05:51:34 +0000

    Modernise PastePipe

--- PastePipe.nix
+++ PastePipe.nix
@@ -1,29 +1,3 @@
-{ supportedPlatforms ? [ "i686-linux" "x86_64-linux" ]
-, supportedCompilers ? [ "ghc763" "ghc783" ]
-}:
+{ utils ? import ./utils.nix {} }:
 
-let
-  genAttrs = (import <nixpkgs> {}).lib.genAttrs;
-in
-rec {
-  PastePipe = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-    in
-     haskellPackages.cabal.mkDerivation (self: {
-      pname = "PastePipe";
-      version = "1.5";
-      src = <PastePipe>;
-      isLibrary = true;
-      isExecutable = true;
-      buildDepends = with haskellPackages; [ cmdargs HTTP network ];
-      meta = {
-        homepage = "http://github.com/creswick/pastepipe";
-        description = "CLI for pasting to lpaste.net";
-        license = "GPL";
-        platforms = self.ghc.meta.platforms;
-        maintainers = with self.stdenv.lib.maintainers; [ fuuzetsu ];
-      };
-    })));
-}
+{ PastePipe = utils.haskellWithDefaults (<localexprs> + "/PastePipe") <PastePipe>; }
