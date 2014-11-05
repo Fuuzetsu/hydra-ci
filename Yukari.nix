21a8ae6a3855dd3b0900efb3cf99b744b6d9ef4b
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 11:45:06 +0000

    yukari

--- Yukari.nix
+++ Yukari.nix
@@ -1,32 +1,3 @@
-{ supportedPlatforms ? [ "i686-linux" "x86_64-linux" ]
-, supportedCompilers ? [ "ghc763" "ghc783" ]
-}:
+{ utils ? import ./utils.nix {} }:
 
-let
-  genAttrs = (import <nixpkgs> {}).lib.genAttrs;
-in
-rec {
-  Yukari = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-    in
-     haskellPackages.cabal.mkDerivation (self: {
-      pname = "Yukari";
-      version = "0.1.0.0";
-      src = <Yukari>;
-      isLibrary = true;
-      isExecutable = true;
-      buildDepends = with haskellPackages; [
-        attoparsec curl downloadCurl dyre filepath HandsomeSoup HTTP hxt
-        lens network text
-      ];
-      testDepends = with haskellPackages; [ filepath hspec lens QuickCheck ];
-      meta = {
-        homepage = "http://github.com/Fuuzetsu/yukari";
-        description = "Command line program that allows for automation of various tasks on the AnimeBytes private tracker website";
-        license = self.stdenv.lib.licenses.gpl3;
-        platforms = self.ghc.meta.platforms;
-      };
-    })));
-}
+{ Yukari = utils.haskellWithDefaults (<localexprs> + "/Yukari") <yukari>; }
