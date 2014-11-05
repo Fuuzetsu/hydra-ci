fc630baf689d4c0b3a863ab5e7c55a633fffb0fd
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 09:26:38 +0000

    use VerSet for Haddock

--- haddock.nix
+++ haddock.nix
@@ -1,17 +1,19 @@
 { utils ? import ./utils.nix {} }:
 
 rec {
-  haddock = utils.haskellFromLocalWithVer
+  haddock = utils.haskellFromLocalWithVerSet
     utils.ghc783Only
     utils.defaultPlatforms
     (<localexprs> + "/haddock")
-    (utils.withExtraBuildInputs <haddock-repo> [ haddockApi ]);
+    (utils.setSrc <haddock-repo>)
+    (self: super: { haddockApi = haddockApi; });
 
-  haddockApi = utils.haskellFromLocalWithVer
+  haddockApi = utils.haskellFromLocalWithVerSet
     utils.ghc783Only
     utils.defaultPlatforms
     (<localexprs> + "/haddock-api")
-    (utils.withExtraBuildInputs (<haddock-repo> + "/haddock-api") [ haddockLibrary ]);
+    (utils.setSrc (<haddock-repo> + "/haddock-api"))
+    (self: super: { haddockLibrary = haddockLibrary; });
 
   haddockLibrary = utils.haskellFromLocal
     [ "ghc742" "ghc763" "ghc783" ]
