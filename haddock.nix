1e405e7edb4f63cf2bd7172ae6a7bf2b07b31520
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-11-05 06:38:33 +0000

    Adapt Haddock jobs

--- haddock.nix
+++ haddock.nix
@@ -1,58 +1,21 @@
-{ supportedPlatforms ? [ "i686-linux" "x86_64-linux" ]
-, supportedCompilers ? [ "ghc742" "ghc763" "ghc783" ]
-, utils ? import ./utils.nix {}
-}:
+{ utils ? import ./utils.nix {} }:
 
-let
-  genAttrs = (import <nixpkgs> {}).lib.genAttrs;
-  helpers = utils;
-in
 rec {
+  haddock = utils.haskellFromLocalWithVer
+    utils.ghc783Only
+    utils.defaultPlatforms
+    (<localexprs> + "/haddock")
+    (utils.withExtraBuildInputs <haddock-repo> [ haddockApi ]);
 
-  haddock = genAttrs [ "ghc783" ] (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-      haddockApiJob = pkgs.lib.getAttrFromPath [ ghcVer system ] haddockApi;
-    in
-    haskellPackages.cabal.mkDerivation (self: rec {
-      pname = "haddock";
-      version = helpers.getCabalVersion (src + "/haddock.cabal");
-      src = <haddock-repo>;
-      buildDepends = with haskellPackages; [ haddockApiJob ];
-      testDepends = with haskellPackages; [ Cabal deepseq filepath hspec QuickCheck ];
-      isLibrary = false;
-      isExecutable = true;
-      enableSplitObjs = false;
-      noHaddock = false;
-      preCheck = "unset GHC_PACKAGE_PATH";
-    })));
+  haddockApi = utils.haskellFromLocalWithVer
+    utils.ghc783Only
+    utils.defaultPlatforms
+    (<localexprs> + "/haddock-api")
+    (utils.withExtraBuildInputs (<haddock-repo> + "/haddock-api") [ haddockLibrary ]);
 
   haddockLibrary = utils.haskellFromLocal
     [ "ghc742" "ghc763" "ghc783" ]
     utils.defaultPlatforms
     (<localexprs> + "/haddock-library")
     (attrs: { src = <haddock-repo> + "/haddock-library"; });
-
-  haddockApi = genAttrs [ "ghc783" ] (ghcVer: genAttrs supportedPlatforms (system:
-    let
-      pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-      haddockLibraryJob = pkgs.lib.getAttrFromPath [ ghcVer system ] haddockLibrary;
-    in
-    haskellPackages.cabal.mkDerivation (self: rec {
-      pname = "haddock-api";
-      version = helpers.getCabalVersion (src + "/haddock-api.cabal");
-      src = <haddock-repo> + "/haddock-api";
-      buildDepends = with haskellPackages;
-                       [ Cabal deepseq filepath ghcPaths xhtml haddockLibraryJob
-                         pkgs.autoconf pkgs.libxslt pkgs.libxml2 pkgs.texLive
-                       ];
-      testDepends = with haskellPackages; [ Cabal deepseq filepath hspec QuickCheck ];
-      isLibrary = true;
-      enableSplitObjs = false;
-      noHaddock = false;
-      preCheck = "unset GHC_PACKAGE_PATH";
-    })));
-
 }
--- utils.nix
+++ utils.nix
@@ -11,19 +11,32 @@ let strs = pkgs.lib.strings;
 inherit (builtins) readFile head filter;
 
 in rec {
-  defaultPlatforms = [ "i686-linux" "x86_64-linux" ];
-  defaultCompilers = [ "ghc763" "ghc783" ];
   ghc763Only = [ "ghc763" ];
+  ghc763Only = [ "ghc763" ];
+  defaultPlatforms = [ "i686-linux" "x86_64-linux" ];
+  defaultCompilers = ghc763Only ++ ghc783Only;
+
+  # Shorthand for common pattern
+  withExtraBuildInputs = srcLoc: ps: ghcVer: system: exprPkgs: attrs: {
+    src = srcLoc;
+    buildInputs = map (exprPkgs.lib.getAttrFromPath [ ghcVer system ]) ps ++ attrs.buildInputs;
+  };
 
   getCabalVersion = file: extractVersion (findVersion (lines (readFile file)));
 
-  # Generate Haskell derivation from given compilers, platforms,
-  # expression path and overrides.
-  haskellFromLocal = comps: plats: exprLoc: overrides: genAttrs comps (ghcVer: genAttrs plats (system:
+  haskellFromLocalWithVer = comps: plats: exprLoc: overrides: genAttrs comps (ghcVer: genAttrs plats (system:
     let
       exprPkgs = import <nixpkgs> { inherit system; };
       haskellPackages = exprPkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] exprPkgs;
-    in exprPkgs.lib.overrideDerivation (haskellPackages.callPackage exprLoc {}) overrides));
+    in exprPkgs.lib.overrideDerivation
+         (haskellPackages.callPackage exprLoc {})
+         (overrides ghcVer system exprPkgs)
+  ));
+
+  # Generate Haskell derivation from given compilers, platforms,
+  # expression path and overrides.
+  haskellFromLocal = comps: plats: exprLoc: overrides:
+    haskellFromLocalWithVer comps plats exprLoc (ghcVer: system: exprPkgs: overrides);
 
  # Default compilers and platforms, overriding src only.
  haskellWithDefaults = exprLoc: srcLoc:
