4b268ac5f8e68c43df434303806e60a3e1d88ff6
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2015-05-27 04:22:56 +0100

    bencoding expr update

--- bittorrent.nix
+++ bittorrent.nix
@@ -1,5 +1,5 @@
 { supportedPlatforms ? [ "i686-linux" "x86_64-linux" ]
-, supportedCompilers ? [ "ghc763" "ghc784" ]
+, supportedCompilers ? [ "ghc784" "ghc7101" ]
 }:
 
 let
@@ -34,20 +34,14 @@ rec {
   bencoding = genAttrs supportedCompilers (ghcVer: genAttrs supportedPlatforms (system:
     let
       pkgs = import <nixpkgs> { inherit system; };
-      haskellPackages =  pkgs.lib.getAttrFromPath ["haskellPackages_${ghcVer}"] pkgs;
-    in
-    haskellPackages.cabal.mkDerivation (self: {
-      pname = "bencoding";
-      version = "HEAD";
-      src = <bencoding>;
-      buildDepends = with haskellPackages; [ attoparsec deepseq mtl text ];
-      testDepends = with haskellPackages; [ attoparsec hspec QuickCheck ];
-      meta = {
-        homepage = "https://github.com/cobit/bencoding";
-        description = "A library for encoding and decoding of BEncode data";
-        license = self.stdenv.lib.licenses.bsd3;
-        platforms = self.ghc.meta.platforms;
-      };
-    })));
+      haskellPackages =  pkgs.lib.getAttrFromPath ["haskell.packages.${ghcVer}"] pkgs;
+    in pkgs.lib.overrideDerivation
+       (haskellPackages.bencoding.override {
+         mkDerivation = (attrs : haskellPackages.mkDerivation (attrs // {
+           version = "HEAD";
+         }));
+       })
+       (attrs: { src = <bencoding>; })
+  ));
 
 }
